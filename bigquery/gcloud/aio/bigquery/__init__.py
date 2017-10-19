e187395c579c987ea3fc925706cbdf9ac5d8d63e
Kevin James <kevin@talkiq.com>
2017-10-18 22:05:27 -0700

    test(bigquery): set up smoke test

--- bigquery/gcloud/aio/bigquery/__init__.py
+++ bigquery/gcloud/aio/bigquery/__init__.py
@@ -1,7 +1,8 @@
 from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-bigquery').version
 
+from gcloud.aio.bigquery.bigquery import make_stream_insert
 from gcloud.aio.bigquery.bigquery import Table
 
 
-__all__ = ['__version__', 'Table']
+__all__ = ['__version__', 'make_stream_insert', 'Table']
