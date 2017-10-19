7ef3503bad3fe9d7d0bef89d1a43f5f6ac133a7e
Kevin James <kevin@talkiq.com>
2017-10-18 22:22:48 -0700

    test(storage): set up smoke test

--- storage/gcloud/aio/storage/__init__.py
+++ storage/gcloud/aio/storage/__init__.py
@@ -3,7 +3,8 @@ __version__ = get_distribution('gcloud-aio-storage').version
 
 from gcloud.aio.storage.storage import Blob
 from gcloud.aio.storage.storage import Bucket
+from gcloud.aio.storage.storage import make_download
 from gcloud.aio.storage.storage import Storage
 
 
-__all__ = ['__version__', 'Blob', 'Bucket', 'Storage']
+__all__ = ['__version__', 'Blob', 'Bucket', 'make_download', 'Storage']
