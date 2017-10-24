527f148d87bdd2e93bff1b492f480317f3b75a8c
Kevin James <kevin@talkiq.com>
2017-10-24 14:25:38 -0700

    feat(taskqueue): export taskqueue.deserialize_task

--- taskqueue/gcloud/aio/taskqueue/__init__.py
+++ taskqueue/gcloud/aio/taskqueue/__init__.py
@@ -1,11 +1,12 @@
 from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-taskqueue').version
 
+from gcloud.aio.taskqueue.taskmanager import deserialize_task
 from gcloud.aio.taskqueue.taskmanager import FailFastError
 from gcloud.aio.taskqueue.taskmanager import TaskManager
 from gcloud.aio.taskqueue.taskqueue import LocalTaskQueue
 from gcloud.aio.taskqueue.taskqueue import TaskQueue
 
 
-__all__ = ['__version__', 'FailFastError', 'TaskManager', 'LocalTaskQueue',
-           'TaskQueue']
+__all__ = ['__version__', 'deserialize_task', 'FailFastError', 'TaskManager',
+           'LocalTaskQueue', 'TaskQueue']
