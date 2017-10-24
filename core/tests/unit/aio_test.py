a445f4be2d476e06b3db199cdb16f2562a6e8aa8
Kevin James <kevin@talkiq.com>
2017-10-24 14:36:15 -0700

    test(core): fix test path for utils.aio -> aio migration

---
+++ core/tests/unit/aio_test.py
@@ -0,0 +1,5 @@
+import gcloud.aio.core.aio as aio  # pylint: disable=unused-import
+
+
+def test_importable():
+    assert True
--- core/tests/unit/utils_aio_test.py
+++
@@ -1,5 +0,0 @@
-import gcloud.aio.core.utils.aio as aio  # pylint: disable=unused-import
-
-
-def test_importable():
-    assert True
