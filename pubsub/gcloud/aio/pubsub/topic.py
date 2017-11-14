ad92f6c71154193e5507459bc968b8cc13997837
Kevin James <kevin@talkiq.com>
2017-11-13 16:49:29 -0800

    feat(pubsub): add create_if_missing abstraction for topics and subscriptions

--- pubsub/gcloud/aio/pubsub/topic.py
+++ pubsub/gcloud/aio/pubsub/topic.py
@@ -1,9 +1,20 @@
 import google.cloud.pubsub as pubsub
+import google.gax.errors
+import grpc
 from gcloud.aio.pubsub.subscription import Subscription
 
 
 class Topic(pubsub.topic.Topic):
-    # pylint: disable=too-few-public-methods
+    def create_if_missing(self, client=None):
+        if self.exists(client=client):
+            return
+
+        try:
+            self.create(client=client)
+        except google.gax.errors.RetryError as e:
+            if e.cause._state != grpc.StatusCode.ALREADY_EXISTS:  # pylint: disable=protected-access
+                raise
+
     def subscription(self, name, ack_deadline=None, push_endpoint=None,
                      retain_acked_messages=None,
                      message_retention_duration=None):
