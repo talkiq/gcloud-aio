e431dbcaafa4d2c386d1331ae2d2390a95902f4c
Kevin James <kevin@talkiq.com>
2018-05-02 11:24:33 -0700

    docs(readme): use gcloud-aio-auth for versions badge

--- auth/nox.py
+++ auth/nox.py
@@ -20,7 +20,7 @@ def unit_tests(session, python_version):
         '--cov=tests.unit',
         '--cov-append',
         '--cov-report=',
-        '--cov-fail-under=39',
+        '--cov-fail-under=38',
         os.path.join('tests', 'unit'),
         *session.posargs)
 
