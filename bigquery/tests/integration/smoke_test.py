d8c88a2fe78298bbf749293f7df541ad19de23e7
Kevin James <kevin@talkiq.com>
2018-05-14 14:55:02 -0700

    chore(bigquery): use pytest-asyncio

--- bigquery/tests/integration/smoke_test.py
+++ bigquery/tests/integration/smoke_test.py
@@ -1,21 +1,13 @@
-import asyncio
 import os
 import uuid
 
 import aiohttp
+import pytest
 from gcloud.aio.bigquery import make_stream_insert
 
 
-async def insert_data(project, creds, dataset_name, table_name, rows):
-    async with aiohttp.ClientSession() as session:
-        stream_insert = make_stream_insert(project, creds, dataset_name,
-                                           table_name, session=session)
-        result = await stream_insert(rows)
-
-    assert result
-
-
-def test_data_is_inserted():
+@pytest.mark.asyncio
+async def test_data_is_inserted():
     project = os.environ['GCLOUD_PROJECT']
     creds = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
     dataset_name = 'test'
@@ -23,6 +15,9 @@ def test_data_is_inserted():
     rows = [{'key': uuid.uuid4().hex, 'value': uuid.uuid4().hex}
             for i in range(3)]
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        insert_data(project, creds, dataset_name, table_name, rows))
+    async with aiohttp.ClientSession() as session:
+        stream_insert = make_stream_insert(project, creds, dataset_name,
+                                           table_name, session=session)
+        result = await stream_insert(rows)
+
+    assert result
