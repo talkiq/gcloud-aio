4919f8565621c980cfd1544c211e88cb64cee48f
Kevin James <kevin@talkiq.com>
2018-05-14 14:57:51 -0700

    chore(datastore): use pytest-asyncio

--- datastore/tests/integration/smoke_test.py
+++ datastore/tests/integration/smoke_test.py
@@ -1,11 +1,18 @@
-import asyncio
 import os
 import uuid
 
+import pytest
 from gcloud.aio.datastore import Datastore
 
 
-async def do_item_lifecycle(project, creds, kind_name, object_name):
+@pytest.mark.asyncio
+async def test_item_lifecycle():
+    project = os.environ['GCLOUD_PROJECT']
+    creds = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+    kind_name = 'gcloud-aio-test'
+    object_name = f'test_record_{uuid.uuid4()}'
+
     # TODO: need a `ds.get` to test these properly
     ds = Datastore(project, creds)
 
@@ -17,15 +24,3 @@ async def do_item_lifecycle(project, creds, kind_name, object_name):
 
     props = {'meaning_of_life': 42}
     await ds.upsert(kind_name, object_name, props)
-
-
-def test_item_lifecycle():
-    project = os.environ['GCLOUD_PROJECT']
-    creds = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
-
-    kind_name = 'gcloud-aio-test'
-    object_name = f'test_record_{uuid.uuid4()}'
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        do_item_lifecycle(project, creds, kind_name, object_name))
