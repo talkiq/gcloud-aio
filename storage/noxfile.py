c1ddfb92f4ed24b268db2237a25606bbc5a85d28
JensMadsen <plasmajens@gmail.com>
2018-11-22 09:19:37 +0100

    adding checks of all env vars required to run integration tests

--- storage/noxfile.py
+++ storage/noxfile.py
@@ -25,7 +25,11 @@ def unit_tests(session):
 @nox.session(python=['3.7'])
 def integration_tests(session):
     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
-        session.skip('Credentials must be set via environment variable.')
+        session.skip('Credentials must be set via the environment variable "GOOGLE_APPLICATION_CREDENTIALS".')
+    if not os.environ.get('BUCKET_NAME', ''):
+        session.skip('Gcloud bucket name must be set via the environment variable: "BUCKET_NAME".')
+    if not os.environ.get('GCLOUD_PROJECT', ''):
+        session.skip('Gcloud project id must be set via the environment variable: "GCLOUD_PROJECT".')
 
     session.install('aiohttp', 'pytest', 'pytest-asyncio', *LOCAL_DEPS)
     session.install('.')
