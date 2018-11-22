4eca4bb08d22965af578911fd425e4dd71e129fd
JensMadsen <plasmajens@gmail.com>
2018-11-22 09:16:38 +0100

    read bucket name from environment variable

--- storage/tests/integration/smoke_test.py
+++ storage/tests/integration/smoke_test.py
@@ -8,6 +8,7 @@ from gcloud.aio.storage import Storage
 
 PROJECT = os.environ['GCLOUD_PROJECT']
 CREDS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+BUCKET_NAME = os.environ['BUCKET_NAME']
 
 
 @pytest.mark.asyncio
@@ -16,25 +17,24 @@ CREDS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
     (json.dumps({'data': 1}), json.dumps({'data': 1}), 'application/json'),
 ])
 async def test_object_life_cycle(uploaded_data, expected_data, content_type):
-    bucket_name = 'talkiq-integration-test'
     object_name = f'{uuid.uuid4().hex}/{uuid.uuid4().hex}.txt'
 
     async with aiohttp.ClientSession() as session:
         storage = Storage(PROJECT, CREDS, session=session)
-        await storage.upload(bucket_name, object_name, uploaded_data, content_type)
+        await storage.upload(BUCKET_NAME, object_name, uploaded_data, content_type)
 
-        bucket = storage.get_bucket(bucket_name)
+        bucket = storage.get_bucket(BUCKET_NAME)
         blob = await bucket.get_blob(object_name)
         contructed_result = await blob.download()
 
         assert contructed_result == expected_data
 
-        direct_result = await storage.download(bucket_name,
+        direct_result = await storage.download(BUCKET_NAME,
                                                object_name)
 
         assert direct_result == expected_data
 
-        await storage.delete(bucket_name, object_name)
+        await storage.delete(BUCKET_NAME, object_name)
 
         with pytest.raises(aiohttp.client_exceptions.ClientResponseError):
-            await storage.download(bucket_name, object_name)
+            await storage.download(BUCKET_NAME, object_name)
