4a9d7a32da337ccdda3e8742f920f6e5239588bb
JensMadsen <plasmajens@gmail.com>
2018-11-23 13:04:32 +0100

    feat(storage): integration tests of str and binary stream uploads

--- storage/tests/integration/upload_resumable_test.py
+++ storage/tests/integration/upload_resumable_test.py
@@ -1,5 +1,8 @@
+import io
 import os
 import uuid
+import string
+import random
 import json
 import aiohttp
 import pytest
@@ -30,3 +33,44 @@ async def test_upload_resumable(data_to_upload, expected_data, file_extension):
 
         assert expected_data == downloaded_data
         await storage.delete(BUCKET_NAME, res['name'])
+
+
+@pytest.mark.asyncio
+async def test_upload_string_stream():
+    object_name = f'{uuid.uuid4().hex}/{uuid.uuid4().hex}.txt'
+    count_chars = 1054
+    random_string = ''.join(random.choice(string.ascii_letters)
+                            for _ in range(count_chars))
+    stream = io.StringIO(random_string)
+
+    async with aiohttp.ClientSession() as session:
+        storage = Storage(PROJECT, CREDS, session=session)
+        res = await storage.upload(BUCKET_NAME, object_name, stream,
+                                   force_resumable_upload=True)
+
+        downloaded_data = await storage.download(BUCKET_NAME, res['name'])
+
+        assert downloaded_data == random_string
+
+        await storage.delete(BUCKET_NAME, res['name'])
+
+
+@pytest.mark.asyncio
+async def test_upload_binary_stream():
+    object_name = f'{uuid.uuid4().hex}/{uuid.uuid4().hex}.bin'
+    file_size = 2045
+    content = os.urandom(file_size)
+    stream = io.BytesIO(content)
+
+    async with aiohttp.ClientSession() as session:
+        storage = Storage(PROJECT, CREDS, session=session)
+        res = await storage.upload(BUCKET_NAME, object_name, stream,
+                                   force_resumable_upload=True)
+
+        downloaded_data = await storage.download(BUCKET_NAME, res['name'])
+
+        assert downloaded_data == content
+
+        await storage.delete(BUCKET_NAME, res['name'])
+
+
