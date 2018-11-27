444ba32f8dded02e2c3a6aafd2a41a7715b730e6
JensMadsen <plasmajens@gmail.com>
2018-11-27 14:42:09 +0100

    test flow with many concurrent tasks

--- storage/tests/integration/upload_resumable_test.py
+++ storage/tests/integration/upload_resumable_test.py
@@ -4,6 +4,7 @@ import uuid
 import string
 import random
 import json
+import asyncio
 import aiohttp
 import pytest
 
@@ -74,3 +75,39 @@ async def test_upload_binary_stream():
         await storage.delete(BUCKET_NAME, res['name'])
 
 
+@pytest.mark.asyncio
+async def test_upload_many(event_loop):
+    def generate_streams(n_streams: int)->list:
+        streams = []
+        for n in range(0, n_streams):
+            object_name = f'{uuid.uuid4().hex}/{uuid.uuid4().hex}.txt'
+            file_size = 2045 * 102
+            content = ''.join(random.choice(string.ascii_letters)
+                              for _ in range(file_size))
+            stream = io.StringIO(content)
+            streams.append((object_name, stream))
+
+        return streams
+
+    streams = generate_streams(5)
+
+    async with aiohttp.ClientSession() as session:
+        storage = Storage(PROJECT, CREDS, session=session)
+        upload_coros = [storage.upload(BUCKET_NAME, s[0], s[1],
+                                       force_resumable_upload=True) for s in streams]
+
+        upload_results = await asyncio.gather(*upload_coros)
+
+        download_coros = [storage.download(
+            BUCKET_NAME, res['name']) for res in upload_results]
+        download_results = await asyncio.gather(*download_coros)
+
+        for stream, download_res in zip(streams, download_results):
+            stream[1].seek(0)
+            assert download_res == stream[1].read()
+
+        delete_coros = [storage.delete(BUCKET_NAME, res['name'])
+                        for res in upload_results]
+        delete_results = await asyncio.gather(*delete_coros)
+        for delete_result in delete_results:
+            assert delete_result == ''
