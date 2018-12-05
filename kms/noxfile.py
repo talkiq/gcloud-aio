90f1bc18af16abd47fb0b2a4c5eb88a5fd1fae76
Kevin James <kevin@talkiq.com>
2018-12-04 21:48:23 -0800

    chore(ci): bump nox version

---
+++ kms/noxfile.py
@@ -0,0 +1,31 @@
+import os
+
+import nox
+
+
+LOCAL_DEPS = ('../auth/', )
+
+
+@nox.session(python=['3.6', '3.7'])
+def unit_tests(session):
+    session.install('pytest', 'pytest-cov', *LOCAL_DEPS)
+    session.install('-e', '.')
+
+    session.run('py.test', '--quiet', '--cov=gcloud.aio.kms',
+                '--cov=tests.unit', '--cov-append', '--cov-report=',
+                os.path.join('tests', 'unit'), *session.posargs)
+
+
+@nox.session(python=['3.7'])
+def lint_setup_py(session):
+    session.install('docutils', 'Pygments')
+    session.run('python', 'setup.py', 'check', '--restructuredtext',
+                '--strict')
+
+
+@nox.session(python=['3.7'])
+def cover(session):
+    session.install('coverage', 'pytest-cov')
+
+    session.run('coverage', 'report', '--show-missing')
+    session.run('coverage', 'erase')
