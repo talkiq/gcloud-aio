c31906ada00b36efa1e168c5b2bb60ad5bb87cbc
Kevin James <kevin@talkiq.com>
2018-12-17 18:14:28 -0800

    feat(datastore): properly expose Mode and Operation enums

--- datastore/gcloud/aio/datastore/__init__.py
+++ datastore/gcloud/aio/datastore/__init__.py
@@ -1,9 +1,12 @@
 from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-datastore').version
 
+from gcloud.aio.datastore.constants import Mode
+from gcloud.aio.datastore.constants import Operation
 from gcloud.aio.datastore.datastore import Datastore
 from gcloud.aio.datastore.key import Key
 from gcloud.aio.datastore.key import PathElement
 
 
-__all__ = ['__version__', 'Datastore', 'Key', 'PathElement']
+__all__ = ['__version__', 'Datastore', 'Key', 'Mode', 'Operation',
+           'PathElement']
