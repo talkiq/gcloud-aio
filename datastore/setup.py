13b9c42e232710d230c13b271c7a850864a547bb
Kevin James <kevin@talkiq.com>
2018-12-17 18:35:09 -0800

    chore(release): bump version

--- datastore/setup.py
+++ datastore/setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='gcloud-aio-datastore',
-    version='1.1.0',
+    version='1.1.1',
     description='Asyncio Python Client for Google Cloud Datastore',
     long_description=README,
     namespace_packages=[
