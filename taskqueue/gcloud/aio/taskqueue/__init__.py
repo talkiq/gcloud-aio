ced5c5795d3ae4cf2aa9b7bfae99a5d8cbbb01af
Kevin Lee <klee@dialpad.com>
2019-01-18 16:47:38 -0800

    refactor(taskqueue): code refactoring
    split queue classes & tests into separate files and
    refactored queue class constructors

--- taskqueue/gcloud/aio/taskqueue/__init__.py
+++ taskqueue/gcloud/aio/taskqueue/__init__.py
@@ -2,12 +2,12 @@ from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-taskqueue').version
 
 from gcloud.aio.taskqueue.pullqueue import PullQueue
+from gcloud.aio.taskqueue.pushqueue import PushQueue
 from gcloud.aio.taskqueue.taskmanager import FailFastError
 from gcloud.aio.taskqueue.taskmanager import TaskManager
-from gcloud.aio.taskqueue.taskqueue import PullQueue, PushQueue
 from gcloud.aio.taskqueue.utils import decode
 from gcloud.aio.taskqueue.utils import encode
 
 
-__all__ = ['__version__', 'decode', 'encode', 'FailFastError', 'TaskManager',
-           'PullQueue', 'PushQueue']
+__all__ = ['__version__', 'decode', 'encode', 'FailFastError',
+           'PullQueue', 'PushQueue', 'TaskManager']
---
+++ taskqueue/tests/integration/pushqueue_test.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from datetime import timedelta
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_task_lifecycle_in_push_queue(push_queue_context):
+    tq = push_queue_context['queue']
+
+    # set to run in the future, giving us enough time to test all functionalities
+    # before the task gets dispatched automatically.
+    schedule_time = datetime.utcnow() + timedelta(days=1)
+    schedule_time = schedule_time.isoformat('T') + 'Z'
+
+    task = {
+        'scheduleTime': schedule_time,
+        'appEngineHttpRequest': {
+            'httpMethod': 'POST',
+            # something that we know won't work,
+            # so that 'run' task operation doesn't end up deleting the task.
+            'relativeUri': '/some/test/uri',
+        }
+    }
+
+    # CREATE
+    created = await tq.create(task)
+    assert created
+
+    # add created task(and the queue to delete it from) to the tasks list,
+    # so that regardless of what happens, the teardown will clean it up.
+    push_queue_context['tasks_to_cleanup'].append(created)
+
+    # GET
+    assert created == await tq.get(created['name'], full=True)
+
+    # LIST
+    listed = await tq.list(full=True)
+    assert listed.get('tasks')
+    assert created in listed['tasks']
+
+    # RUN
+    run = await tq.run(created['name'], full=True)
+    assert all(item in run.items() for item in created.items())
+
+    # DELETE
+    assert not await tq.delete(created['name'])
+
+    # created task has been deleted successfully,
+    # so remove it from the list to avoid unnecessary delete attempt
+    push_queue_context['tasks_to_cleanup'].remove(created)
