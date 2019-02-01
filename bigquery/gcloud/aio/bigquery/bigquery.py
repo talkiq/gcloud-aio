3d10156463141d3d78cc3d817f108134d927f218
Kevin James <kevin@talkiq.com>
2019-01-31 16:17:03 -0800

    feat(bigquery): make project and service_file optional

--- bigquery/gcloud/aio/bigquery/bigquery.py
+++ bigquery/gcloud/aio/bigquery/bigquery.py
@@ -1,17 +1,20 @@
 import functools
 import logging
 import uuid
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
 
 import aiohttp
 from gcloud.aio.auth import Token  # pylint: disable=no-name-in-module
 try:
     import ujson as json
 except ModuleNotFoundError:
-    import json
+    import json  # type: ignore
 
 
 API_ROOT = 'https://www.googleapis.com/bigquery/v2'
-INSERT_TEMPLATE = 'projects/{proj}/datasets/{dataset}/tables/{table}/insertAll'
 SCOPES = [
     'https://www.googleapis.com/auth/bigquery.insertdata'
 ]
@@ -34,42 +37,49 @@ def new_insert_id():
     return uuid.uuid4().hex
 
 
-def make_rows(rows):
-
-    bq_rows = [{
+def make_rows(rows: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    return [{
         'insertId': new_insert_id(),
         'json': row
     } for row in rows]
 
-    return bq_rows
-
 
 class Table:
-    def __init__(self, project, service_file, dataset_name, table_name,
-                 session=None, token=None):
-        self.project = project
-        self.table_name = table_name
+    def __init__(self, dataset_name: str, table_name: str,
+                 project: Optional[str] = None,
+                 service_file: Optional[str] = None,
+                 session: Optional[aiohttp.ClientSession] = None,
+                 token: Optional[Token] = None) -> None:
+        self._project = project
         self.dataset_name = dataset_name
+        self.table_name = table_name
 
         self.session = session
-        self.token = token or Token(project, service_file, session=session,
+        self.token = token or Token(service_file=service_file, session=session,
                                     scopes=SCOPES)
 
-    async def headers(self):
+    async def project(self) -> str:
+        if self._project:
+            return self._project
 
+        self._project = self.token.get_project()
+        if self._project:
+            return self._project
+
+        raise Exception('could not determine project, please set it manually')
+
+    async def headers(self) -> Dict[str, str]:
         token = await self.token.get()
-
         return {
             'Authorization': f'Bearer {token}',
         }
 
-    async def insert(self, rows, skip_invalid=False, ignore_unknown=True,
-                     session=None):
-        insert_url = INSERT_TEMPLATE.format(proj=self.project,
-                                            dataset=self.dataset_name,
-                                            table=self.table_name)
-        url = f'{API_ROOT}/{insert_url}'
-        log.info('Inserting %d rows to %s', len(rows), url)
+    async def insert(self, rows: List[Dict[str, Any]],
+                     skip_invalid: bool = False, ignore_unknown: bool = True,
+                     session: Optional[aiohttp.ClientSession] = None):
+        project = await self.project()
+        url = (f'{API_ROOT}/projects/{project}/datasets/{self.dataset_name}/'
+               f'tables/{self.table_name}/insertAll')
 
         body = make_insert_body(rows, skip_invalid=skip_invalid,
                                 ignore_unknown=ignore_unknown)
@@ -101,22 +111,12 @@ class Table:
 
 
 async def stream_insert(table, rows):
-
-    insert_rows = make_rows(rows)
-    result = await table.insert(insert_rows)
-
-    return result
+    return await table.insert(make_rows(rows))
 
 
-def make_stream_insert(project, service_file, dataset_name, table_name,
-                       session=None):
-
-    table = Table(
-        project,
-        service_file,
-        dataset_name,
-        table_name,
-        session=session
-    )
+def make_stream_insert(dataset_name, table_name, project=None,
+                       service_file=None, session=None):
+    table = Table(dataset_name, table_name, project=project,
+                  service_file=service_file, session=session)
 
     return functools.partial(stream_insert, table)
