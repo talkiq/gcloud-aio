a91c9622b6827174f839aa0715091e78e40b9fcf
Kevin James <kevin@talkiq.com>
2019-02-14 21:34:58 -0800

    feat(storage): upload via streams
    Memory efficient upload method first provided by @JensMadsen in #48.

--- storage/tests/integration/upload_resumable_test.py
+++ storage/tests/integration/upload_resumable_test.py
@@ -1,9 +1,18 @@
+import io
 import json
+import os
+import random
+import string
 import uuid
 
 import aiohttp
 import pytest
 from gcloud.aio.storage import Storage
+
+
+# TODO: use hypothesis
+RANDOM_BINARY = os.urandom(2045)
+RANDOM_STRING = ''.join(random.choices(string.ascii_letters, k=1054))
 
 
 @pytest.mark.asyncio
@@ -12,6 +21,8 @@ from gcloud.aio.storage import Storage
     (json.dumps({'data': 1}), json.dumps({'data': 1}).encode('utf-8'), 'json'),
     (json.dumps([1, 2, 3]), json.dumps([1, 2, 3]).encode('utf-8'), 'json'),
     ('test'.encode('utf-8'), 'test'.encode('utf-8'), 'bin'),
+    (io.BytesIO(RANDOM_BINARY), RANDOM_BINARY, 'bin'),
+    (io.StringIO(RANDOM_STRING), RANDOM_STRING.encode('utf-8'), 'txt'),
 ])
 async def test_upload_resumable(bucket_name, creds, uploaded_data,
                                 expected_data, file_extension):
