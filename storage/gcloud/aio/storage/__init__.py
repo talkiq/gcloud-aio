e4a812ef8c7d87978f2f2f390db05423371a126d
Kevin James <kevin@talkiq.com>
2019-03-21 15:55:03 -0700

    feat(storage): expose token scope requirements

--- storage/gcloud/aio/storage/__init__.py
+++ storage/gcloud/aio/storage/__init__.py
@@ -3,7 +3,8 @@ __version__ = get_distribution('gcloud-aio-storage').version
 
 from gcloud.aio.storage.blob import Blob
 from gcloud.aio.storage.bucket import Bucket
+from gcloud.aio.storage.storage import SCOPES
 from gcloud.aio.storage.storage import Storage
 
 
-__all__ = ['__version__', 'Blob', 'Bucket', 'Storage']
+__all__ = ['__version__', 'Blob', 'Bucket', 'SCOPES', 'Storage']
