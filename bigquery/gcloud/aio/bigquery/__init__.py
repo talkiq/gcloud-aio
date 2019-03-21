a55873d5215d692496eff4bf13f55ede8555783e
Kevin James <kevin@talkiq.com>
2019-03-21 15:53:18 -0700

    feat(bigquery): expose token scope requirements

--- bigquery/gcloud/aio/bigquery/__init__.py
+++ bigquery/gcloud/aio/bigquery/__init__.py
@@ -1,7 +1,8 @@
 from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-bigquery').version
 
+from gcloud.aio.bigquery.bigquery import SCOPES
 from gcloud.aio.bigquery.bigquery import Table
 
 
-__all__ = ['__version__', 'Table']
+__all__ = ['__version__', 'SCOPES', 'Table']
