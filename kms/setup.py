9e020790dd85d313c3676a25f95db4889ef9e215
Kevin James <kevin@talkiq.com>
2019-04-04 14:18:23 -0700

    chore(release): bump version

--- kms/setup.py
+++ kms/setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='gcloud-aio-kms',
-    version='2.0.0',
+    version='2.1.0',
     description='Asyncio Python Client for Google Cloud KMS',
     long_description=README,
     namespace_packages=[
