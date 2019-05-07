0f5411f18098bb555cb9423662ec93f9678c2a01
Kevin Guillaumond <kguillaumond@dialpad.com>
2019-05-07 09:48:56 -0700

    Remove test class for datastore and simplify test

--- datastore/tests/unit/datastore_test.py
+++ datastore/tests/unit/datastore_test.py
@@ -3,20 +3,11 @@ from gcloud.aio.datastore.constants import TypeName
 from gcloud.aio.datastore.utils import TYPES
 
 
-class TestDatastore:
-    @staticmethod
-    def test_defining_custom_subclass_updates_list_of_types():
-        class CustomKey:
-            pass
+def test_defining_custom_subclass_updates_list_of_types():
+    class CustomKey:
+        pass
 
-        class CustomEntity:
-            pass
+    class CustomDatastore(datastore.Datastore):  # pylint: disable=unused-variable
+        key_kind = CustomKey
 
-        class CustomEntityResult(datastore.EntityResult):
-            entity_kind = CustomEntity
-
-        class CustomDatastore(datastore.Datastore):  # pylint: disable=unused-variable
-            key_kind = CustomKey
-            entity_result_kind = CustomEntityResult
-
-        assert TYPES[CustomKey] == TypeName.KEY
+    assert TYPES[CustomKey] == TypeName.KEY
