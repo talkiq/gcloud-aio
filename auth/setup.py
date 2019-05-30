9aed03571ac5695ef721bd81aa802582c0e7300c
Kevin James <kevin@talkiq.com>
2019-05-30 15:42:23 -0700

    chore(release): bump version

--- auth/setup.py
+++ auth/setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='gcloud-aio-auth',
-    version='2.1.0',
+    version='2.1.1',
     description='Auth Helpers for Asyncio Google Cloud Library',
     long_description=README,
     namespace_packages=[
