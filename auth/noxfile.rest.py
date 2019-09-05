647b92d94d9363cb4cdb0638361a826cc3620a1c
Harsh Saini <hsaini@dialpad.com>
2019-09-04 19:01:33 -0700

    Enable integration tests for gcloud-rest

--- auth/noxfile.rest.py
+++ auth/noxfile.rest.py
@@ -14,17 +14,17 @@ def unit_tests(session):
 
 
 # TODO: re-enable once we have some integration tests again
-# @nox.session(python=['2.7', '3.7'], reuse_venv=True)
-# def integration_tests(session):
-#     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
-#         session.skip('Credentials must be set via environment variable.')
+@nox.session(python=['2.7', '3.7'], reuse_venv=True)
+def integration_tests(session):
+    if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
+        session.skip('Credentials must be set via environment variable.')
 
-#     session.install('pytest', 'pytest-cov', 'pytest-mock')
-#     session.install('.')
+    session.install('pytest', 'pytest-cov', 'pytest-mock')
+    session.install('.')
 
-#     session.run('py.test', '--quiet', '--cov=gcloud.rest',
-#                 '--cov=tests.integration', '--cov-append', '--cov-report=',
-#                 os.path.join('tests', 'integration'), *session.posargs)
+    session.run('py.test', '--quiet', '--cov=gcloud.rest',
+                '--cov=tests.integration', '--cov-append', '--cov-report=',
+                os.path.join('tests', 'integration'), *session.posargs)
 
 
 @nox.session(python=['2.7', '3.7'], reuse_venv=True)
