045ff892a5f3e5704acdcfbecedc0676ff1617e6
Harsh Saini <hsaini@dialpad.com>
2019-09-05 08:14:49 -0700

    Fix unicode issue

--- auth/gcloud/aio/auth/utils.py
+++ auth/gcloud/aio/auth/utils.py
@@ -2,11 +2,6 @@ import base64
 import sys
 from typing import Union
 
-def make_compatible_bytes_unicode(text: str):
-    if sys.version_info[0] < 3:
-        return unicode(text)  # pylint:disable=undefined-variable
-    return bytes(text, 'utf-8')
-
 
 def decode(payload: str) -> bytes:
     """
@@ -15,8 +10,12 @@ def decode(payload: str) -> bytes:
 
     See https://en.wikipedia.org/wiki/Base64#URL_applications
     """
-    return base64.b64decode(payload,
-                            altchars=make_compatible_bytes_unicode('-_'))
+
+    if not isinstance(payload, bytes) and sys.version_info[0] < 3:
+        # Base64 encode/decode does not accept `str` as input in python2
+        payload = payload.encode('utf-8')
+
+    return base64.b64decode(bytes(payload, 'utf-8'), altchars=b'-_')
 
 
 def encode(payload: Union[bytes, str]) -> bytes:
@@ -29,5 +28,4 @@ def encode(payload: Union[bytes, str]) -> bytes:
     if isinstance(payload, str):
         payload = payload.encode('utf-8')
 
-    return base64.b64encode(payload,
-                            altchars=make_compatible_bytes_unicode('-_'))
+    return base64.b64encode(payload, altchars=b'-_')
