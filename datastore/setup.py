a75b4b5023deb1f6e270f7b0c10e8953cbe92757
Kevin James <kevin@talkiq.com>
2020-03-26 20:42:54 -0700

    chore: bump versions

--- datastore/setup.py
+++ datastore/setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='gcloud-aio-datastore',
-    version='5.3.0',
+    version='5.4.0',
     description='Python Client for Google Cloud Datastore',
     long_description=README,
     namespace_packages=[
