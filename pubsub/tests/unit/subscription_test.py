f144963d554d7cef22649d74a037bef2e14270e9
eddiedialpad <eddie@dialpad.com>
2020-09-03 18:37:12 -0400

    feat(pubsub): stub unit tests for subscriber classes

--- pubsub/tests/unit/subscription_test.py
+++ pubsub/tests/unit/subscription_test.py
@@ -1,2 +1,21 @@
+from gcloud.aio.auth import BUILD_GCLOUD_REST  # pylint: disable=no-name-in-module
+
+
 def test_importable():
     assert True
+
+
+if BUILD_GCLOUD_REST:
+    pass
+
+
+else:
+    from gcloud.aio.pubsub.subscriber_client import SubscriberClient
+    from gcloud.aio.pubsub.subscriber_message import SubscriberMessage  # pylint: disable=unused-import
+    from gcloud.aio.pubsub.subscriber_message import FlowControl
+
+    def test_construct_subscriber_client():
+        SubscriberClient()
+
+    def test_construct_flow_control():
+        FlowControl()
