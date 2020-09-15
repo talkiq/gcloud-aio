dff295ca72d3ec36b6019a377ccda2ca5ac8ee29
eddiedialpad <eddie@dialpad.com>
2020-09-15 10:57:45 -0400

    temp(pubsub): import fixes from staging - pubsub utils

--- pubsub/gcloud/aio/pubsub/utils.py
+++ pubsub/gcloud/aio/pubsub/utils.py
@@ -1,3 +1,4 @@
+from typing import Any
 from typing import Dict
 from typing import Union
 
@@ -62,14 +63,14 @@ else:
 # https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
 class PubsubMessage:
     def __init__(self, data: Union[bytes, str],
-                 **kwargs: Dict[str, str]) -> None:
+                 **kwargs: Dict[str, Any]) -> None:
         self.data = data
         self.attributes = kwargs
 
     def __repr__(self) -> str:
         return str(self.to_repr())
 
-    def to_repr(self) -> Dict[str, str]:
+    def to_repr(self) -> Dict[str, Any]:
         return {
             'data': encode(self.data).decode('utf-8'),
             'attributes': self.attributes,
