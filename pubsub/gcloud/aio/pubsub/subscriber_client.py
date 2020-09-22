e6da754c834649a8c43ea0cfc786e73a42abca0b
eddiedialpad <41925539+eddiedialpad@users.noreply.github.com>
2020-09-22 09:12:32 -0400

    feat(pubsub): encapsulate Message & FlowControl, nix Scheduler (#201)

--- pubsub/gcloud/aio/pubsub/subscriber_client.py
+++ pubsub/gcloud/aio/pubsub/subscriber_client.py
@@ -1,36 +1,62 @@
 from gcloud.aio.auth import BUILD_GCLOUD_REST  # pylint: disable=no-name-in-module
 
 if BUILD_GCLOUD_REST:
+    class FlowControl:
+        def __init__(self, **kwargs) -> None:
+            raise NotImplementedError('this class is only implemented in aio')
+
     class SubscriberClient:
         def __init__(self, **kwargs) -> None:
             raise NotImplementedError('this class is only implemented in aio')
+
 else:
     import asyncio
     import concurrent.futures
     import signal
     from typing import Any
     from typing import Callable
+    from typing import Dict
+    from typing import List
     from typing import Optional
+    from typing import Tuple
+    from typing import Union
 
     from google.api_core import exceptions
     from google.cloud import pubsub
+    from google.cloud.pubsub_v1.types import FlowControl as _FlowControl
     from google.cloud.pubsub_v1.subscriber.message import Message
-    from google.cloud.pubsub_v1.subscriber.scheduler import Scheduler
-    from google.cloud.pubsub_v1.types import FlowControl
 
+    from .subscriber_message import SubscriberMessage
     from .utils import convert_google_future_to_concurrent_future
+
+
+    class FlowControl:
+        def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+            """
+            FlowControl transitional wrapper.
+            (FlowControl fields docs)[https://github.com/googleapis/python-pubsub/blob/6b9eec81ccee81ab93646eaf7652139fc218ed36/google/cloud/pubsub_v1/types.py#L139-L159]  # pylint: disable=line-too-long
+            Google uses a named tuple; here are the fields, defaults:
+            - max_bytes: int = 100 * 1024 * 1024
+            - max_messages: int = 1000
+            - max_lease_duration: int = 1 * 60 * 60
+            - max_duration_per_lease_extension: int = 0
+            """
+            self._flow_control = _FlowControl(*args, **kwargs)
+
+        def __getitem__(self, index: int) -> int:
+            return self._flow_control[index]
 
 
     class SubscriberClient:
         def __init__(self, *, loop: Optional[asyncio.AbstractEventLoop] = None,
-                     **kwargs: Any) -> None:
+                     **kwargs: Dict[str, Any]) -> None:
             self._subscriber = pubsub.SubscriberClient(**kwargs)
             self.loop = loop or asyncio.get_event_loop()
 
         def create_subscription(self,
                                 subscription: str,
                                 topic: str,
-                                **kwargs
+                                **kwargs: Dict[str, Any]
                                 ) -> None:
             """
             Create subscription if it does not exist. Check out the official
@@ -48,32 +74,31 @@ else:
 
         def subscribe(self,
                       subscription: str,
-                      callback: Callable[[Message], None],
+                      callback: Callable[[SubscriberMessage], Any],
                       *,
-                      flow_control: FlowControl = (),
-                      scheduler: Optional[Scheduler] = None
-                      ) -> asyncio.Future:
+                      flow_control: Union[FlowControl, Tuple[int, ...]] = ()
+                      ) -> asyncio.Future:  # type: ignore
             """
             Create subscription through pubsub client, hijack the returned
             "non-concurrent Future" and coerce it into being a "concurrent
             Future", wrap it into a asyncio Future and return it.
             """
-            sub_keepalive: asyncio.Future = self._subscriber.subscribe(
-                subscription,
-                self._wrap_callback(callback),
-                flow_control=flow_control,
-                scheduler=scheduler
-            )
+            sub_keepalive: asyncio.Future = (  # type: ignore
+                self._subscriber.subscribe(
+                    subscription,
+                    self._wrap_callback(callback),
+                    flow_control=flow_control))
 
             convert_google_future_to_concurrent_future(
-                sub_keepalive, loop=self.loop
-            )
+                sub_keepalive, loop=self.loop)
+
             _ = asyncio.wrap_future(sub_keepalive)
             self.loop.add_signal_handler(signal.SIGTERM, sub_keepalive.cancel)
 
             return sub_keepalive
 
-        def run_forever(self, sub_keepalive: asyncio.Future) -> None:
+        def run_forever(self,
+                        sub_keepalive: asyncio.Future) -> None:  # type: ignore
             """
             Start the asyncio loop, running until it is either SIGTERM-ed or
             killed by keyboard interrupt. The Future parameter is used to
@@ -100,10 +125,14 @@ else:
                 self.loop.stop()
 
         def _wrap_callback(self,
-                           callback: Callable[[Message], None]
+                           callback: Callable[[SubscriberMessage], None]
                            ) -> Callable[[Message], None]:
             """Schedule callback to be called from the event loop"""
             def _callback_wrapper(message: Message) -> None:
-                asyncio.run_coroutine_threadsafe(callback(message), self.loop)
+                asyncio.run_coroutine_threadsafe(
+                    callback(  # type: ignore
+                        SubscriberMessage.from_google_cloud(
+                            message)),
+                    self.loop)
 
             return _callback_wrapper
---
+++ pubsub/gcloud/aio/pubsub/subscriber_message.py
@@ -0,0 +1,83 @@
+from gcloud.aio.auth import BUILD_GCLOUD_REST  # pylint: disable=no-name-in-module
+
+if BUILD_GCLOUD_REST:
+    class SubscriberMessage:
+        def __init__(self, **kwargs) -> None:
+            raise NotImplementedError('this class is only implemented in aio')
+else:
+    import datetime
+
+    from typing import Any
+    from typing import Dict
+    from typing import List
+    from typing import Optional
+
+    from google.cloud.pubsub_v1.subscriber.message import Message
+
+
+    class SubscriberMessage:
+        def __init__(self, *args: List[Any],
+                     google_cloud_message: Message = None,
+                     **kwargs: Dict[str, Any]) -> None:
+            if google_cloud_message:
+                self._message = google_cloud_message
+                return
+            self._message = Message(*args, **kwargs)
+
+        @staticmethod
+        def from_google_cloud(message: Message) -> 'SubscriberMessage':
+            return SubscriberMessage(google_cloud_message=message)
+
+        @property
+        def google_cloud_message(self) -> Message:
+            return self._message
+
+        @property
+        def message_id(self) -> str:  # indirects to a Google protobuff field
+            return str(self._message.message_id)
+
+        def __repr__(self) -> str:
+            return repr(self._message)
+
+        @property
+        def attributes(self) -> Any:  # Google .ScalarMapContainer
+            return self._message.attributes
+
+        @property
+        def data(self) -> bytes:
+            return bytes(self._message.data)
+
+        @property
+        def publish_time(self) -> datetime.datetime:
+            published: datetime.datetime = self._message.publish_time
+            return published
+
+        @property
+        def ordering_key(self) -> str:
+            return str(self._message.ordering_key)
+
+        @property
+        def size(self) -> int:
+            return int(self._message.size)
+
+        @property
+        def ack_id(self) -> str:
+            return str(self._message.ack_id)
+
+        @property
+        def delivery_attempt(self) -> Optional[int]:
+            if self._message.delivery_attempt:
+                return int(self._message.delivery_attempt)
+            return None
+
+        def ack(self) -> None:
+            self._message.ack()
+
+        def drop(self) -> None:
+            self._message.drop()
+
+        def modify_ack_deadline(self, seconds: int) -> None:
+            self._message.modify_ack_deadline(seconds)
+
+        def nack(self) -> None:
+            self._message.nack()
