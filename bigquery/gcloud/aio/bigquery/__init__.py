32e5014100712d44c5b7289c81c1fb26f500ab7e
Aliaksandr Martsinovich <aliaksandr.martsinovich@dialpad.com>
2020-10-13 15:57:45 -0700

    add SchemaUpdateOption to __init__.py

--- bigquery/gcloud/aio/bigquery/__init__.py
+++ bigquery/gcloud/aio/bigquery/__init__.py
@@ -4,7 +4,9 @@ __version__ = get_distribution('gcloud-aio-bigquery').version
 from gcloud.aio.bigquery.bigquery import Disposition
 from gcloud.aio.bigquery.bigquery import SCOPES
 from gcloud.aio.bigquery.bigquery import SourceFormat
+from gcloud.aio.bigquery.bigquery import SchemaUpdateOption
 from gcloud.aio.bigquery.bigquery import Table
 
 
-__all__ = ['__version__', 'Disposition', 'SCOPES', 'SourceFormat', 'Table']
+__all__ = ['__version__', 'Disposition', 'SCOPES', 'SourceFormat',
+           'SchemaUpdateOption', 'Table']
