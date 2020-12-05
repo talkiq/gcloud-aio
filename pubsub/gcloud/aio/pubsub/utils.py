8aba6f010fe122281788f356ce6962a7362fa9b8
Alex <globophobe@gmail.com>
2020-12-06 08:51:57 +0900

    feat(pubsub): added orderingKey to PubsubMessage
    Added orderingKey to PubsubMessage
    https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage

--- pubsub/gcloud/aio/pubsub/utils.py
+++ pubsub/gcloud/aio/pubsub/utils.py
@@ -7,16 +7,20 @@ from gcloud.aio.auth import encode  # pylint: disable=no-name-in-module
 
 # https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
 class PubsubMessage:
-    def __init__(self, data: Union[bytes, str],
+    def __init__(self, data: Union[bytes, str], ordering_key: str = '',
                  **kwargs: Dict[str, Any]) -> None:
         self.data = data
         self.attributes = kwargs
+        self.ordering_key = ordering_key
 
     def __repr__(self) -> str:
         return str(self.to_repr())
 
     def to_repr(self) -> Dict[str, Any]:
-        return {
+        msg = {
             'data': encode(self.data).decode('utf-8'),
             'attributes': self.attributes,
         }
+        if self.ordering_key:
+            msg['orderingKey'] = self.ordering_key
+        return msg
