05d86d5d130022f043cb1b8ce4f643db420547a8
Aliaksandr Martsinovich <aliaksandr.martsinovich@dialpad.com>
2020-12-16 15:50:44 -0800

    fix rest tests

--- pubsub/gcloud/aio/pubsub/__init__.py
+++ pubsub/gcloud/aio/pubsub/__init__.py
@@ -1,12 +1,17 @@
 from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-aio-pubsub').version
 
+from gcloud.aio.auth import BUILD_GCLOUD_REST
 from gcloud.aio.pubsub.publisher_client import PublisherClient
 from gcloud.aio.pubsub.subscriber_client import SubscriberClient
 from gcloud.aio.pubsub.utils import PubsubMessage
 from gcloud.aio.pubsub.subscriber_message import SubscriberMessage
-from gcloud.aio.pubsub.subscriber import subscribe
 
+if BUILD_GCLOUD_REST:
+    __all__ = ['__version__', 'PublisherClient', 'PubsubMessage',
+               'SubscriberClient', 'SubscriberMessage']
+else:
+    from gcloud.aio.pubsub.subscriber import subscribe
 
-__all__ = ['__version__', 'PublisherClient', 'PubsubMessage',
-           'SubscriberClient', 'SubscriberMessage', 'subscribe']
+    __all__ = ['__version__', 'PublisherClient', 'PubsubMessage',
+               'SubscriberClient', 'SubscriberMessage', 'subscribe']
