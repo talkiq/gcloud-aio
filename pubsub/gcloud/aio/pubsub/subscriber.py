575807ec2018b99cb7dc3e573888b79487adcdea
Aliaksandr Martsinovich <62574824+amartsinovich-dialpad@users.noreply.github.com>
2021-01-14 12:00:06 -0800

    Revert "chore(pubsub) Close subscriber session during shutdown" (#285)
    This reverts commit 2817d54d5c2765e5ce4b093b562ade3770bca112 and bumps version to 4.0.4.

--- pubsub/gcloud/aio/pubsub/subscriber.py
+++ pubsub/gcloud/aio/pubsub/subscriber.py
@@ -324,5 +324,4 @@ else:
             for task in acker_tasks:
                 task.cancel()
             await asyncio.wait(acker_tasks, return_when=asyncio.ALL_COMPLETED)
-            await subscriber_client.session.close()
         raise asyncio.CancelledError('Subscriber shut down')
