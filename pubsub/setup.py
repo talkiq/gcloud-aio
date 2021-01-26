bff12d49c68f93190e803c404169583b4e24b17e
Aliaksandr Martsinovich <aliaksandr.martsinovich@dialpad.com>
2021-01-26 10:01:42 -0800

    chore(pubsub): bump to v4.2.1

--- pubsub/setup.py
+++ pubsub/setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
 
 setuptools.setup(
     name='gcloud-aio-pubsub',
-    version='4.2.0',
+    version='4.2.1',
     description='Python Client for Google Cloud Pub/Sub',
     long_description=README,
     namespace_packages=[
