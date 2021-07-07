50f2b1b755c232c16e0c92d3410d7ee137367485
Casey Howard <casey.howard@dialpad.com>
2021-07-07 15:48:19 -0400

    chore(tests): Correct REST-specific tests

--- auth/tests/unit/sessions_test.py
+++ auth/tests/unit/sessions_test.py
@@ -3,7 +3,9 @@ from gcloud.aio.auth.build_constants import BUILD_GCLOUD_REST
 from gcloud.aio.auth.session import AioSession
 
 if BUILD_GCLOUD_REST:
-    from requests import Session
+    from unittest.mock import MagicMock as Session
+    import requests
+    requests.Session = Session
 else:
     from aiohttp import ClientSession as Session
 
@@ -15,7 +17,10 @@ async def test_unmanaged_session():
         assert gcloud_session._shared_session  # pylint: disable=protected-access
         await gcloud_session.close()
 
-        assert not session.closed
+        if BUILD_GCLOUD_REST:
+            session.close.assert_not_called()
+        else:
+            assert not session.closed
 
 
 @pytest.mark.asyncio
@@ -24,4 +29,8 @@ async def test_managed_session():
     internal_session = gcloud_session.session
     assert not gcloud_session._shared_session  # pylint: disable=protected-access
     await gcloud_session.close()
-    assert internal_session.closed
+
+    if BUILD_GCLOUD_REST:
+        gcloud_session.close.assert_called()  # pylint: disable=no-member
+    else:
+        assert internal_session.closed
