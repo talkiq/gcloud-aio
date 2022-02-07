2a3e52761f2531772014cf0ad1469ef8f3af3060
Jonathan Johnston <jonathanj@calabs.ca>
2022-02-07 16:50:57 -0500

    feat(olly): enable prometheus metrics in pubsub

--- pubsub/gcloud/aio/pubsub/metrics_agent.py
+++ pubsub/gcloud/aio/pubsub/metrics_agent.py
@@ -1,3 +1,6 @@
+import prometheus_client
+
+
 class MetricsAgent:
     """
     Any metric client should implement this interface
@@ -12,3 +15,35 @@ class MetricsAgent:
                   metric: str,
                   value: float = 1) -> None:
         pass
+
+
+class PrometheusMetrics:
+    def __init__(self, *, namespace: str = '',
+                 subsystem: str = '') -> None:
+        self.consume = prometheus_client.Counter(
+            'pubsub_consume',
+            'Counter of the outcomes of PubSub message consume attempts',
+            ['outcome'], namespace=namespace, subsystem=subsystem)
+
+        self.consume_latency = prometheus_client.Histogram(
+            'pubsub_consume_latency',
+            'Histogram of PubSub message consume latencies', ['aspect'],
+            namespace=namespace, subsystem=subsystem)
+
+        self.batch_fail = prometheus_client.Counter(
+            'pubsub_batch_failed',
+            'Counter for failures to process PubSub message batches',
+            ['component'], namespace=namespace, subsystem=subsystem)
+
+        self.messages_processed = prometheus_client.Counter(
+            'pubsub_messages_processed',
+            'Counter of successfully processed messages',
+            ['component'], namespace=namespace, subsystem=subsystem)
+
+
+PROMETHEUS = PrometheusMetrics()
+
+
+def configure_prometheus(*, namespace: str = '', subsystem: str = '') -> None:
+    global PROMETHEUS  # pylint: disable=global-statement
+    PROMETHEUS = PrometheusMetrics(namespace=namespace, subsystem=subsystem)
