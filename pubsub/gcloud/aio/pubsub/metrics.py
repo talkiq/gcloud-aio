8083ce6f939a1feeea617d2789309419faf43aa6
Jonathan Johnston <jonathanj@calabs.ca>
2022-02-09 11:11:31 -0500

    refactor(pubsub): convert to namespaced metrics

---
+++ pubsub/gcloud/aio/pubsub/metrics.py
@@ -0,0 +1,39 @@
+import prometheus_client
+from gcloud.aio.auth import BUILD_GCLOUD_REST
+
+
+_NAMESPACE = f'gcloud_{"rest" if BUILD_GCLOUD_REST else "aio"}'
+_SUBSYSTEM = 'pubsub'
+
+
+CONSUME = prometheus_client.Counter(
+    'subscriber_consume',
+    'Counter of the outcomes of PubSub message consume attempts',
+    ['outcome'],
+    namespace=_NAMESPACE,
+    subsystem=_SUBSYSTEM,
+    unit='total')
+
+CONSUME_LATENCY = prometheus_client.Histogram(
+    'subscriber_consume_latency',
+    'Histogram of PubSub message consume latencies',
+    ['aspect'],
+    namespace=_NAMESPACE,
+    subsystem=_SUBSYSTEM,
+    unit='seconds')
+
+BATCH_FAIL = prometheus_client.Counter(
+    'subscriber_batch_failed',
+    'Counter for failures to process PubSub message batches',
+    ['component'],
+    namespace=_NAMESPACE,
+    subsystem=_SUBSYSTEM,
+    unit='total')
+
+MESSAGES_PROCESSED = prometheus_client.Counter(
+    'subscriber_messages_processed',
+    'Counter of successfully processed messages',
+    ['component'],
+    namespace=_NAMESPACE,
+    subsystem=_SUBSYSTEM,
+    unit='total')
