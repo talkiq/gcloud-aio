522b4e01a8a683b931fbe914867a08744fbc5ef0
Tony Au-Yeung <tony@elevenlabs.io>
2025-05-05 13:18:40 -0500

    PR feedback

--- auth/tests/unit/token_test.py
+++ auth/tests/unit/token_test.py
@@ -74,6 +74,9 @@ async def test_acquiring_cancellation(acquire_access_token_mock):
     with pytest.raises(asyncio.TimeoutError):
         await t.get()
 
+    assert t.acquiring.done(), "Acquiring should be done after timeout"
+    assert not t.access_token, "Token should not be set after timeout"
+
     # If the token timed out last time, it should retry instead of trying the
     # timed out coroutine again
     acquire_access_token_mock.side_effect = None
