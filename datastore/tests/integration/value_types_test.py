b79d59c1fbca896281235e9dcad9131f4132f9e8
Kevin James <kevin@talkiq.com>
2026-01-29 16:19:58 +0000

    tests(datastore): fixup bad type assumption

--- datastore/tests/integration/value_types_test.py
+++ datastore/tests/integration/value_types_test.py
@@ -1,29 +1,21 @@
 """Make sure all value types are serialized/deserialized correctly"""
 import pytest
-from gcloud.aio.auth import BUILD_GCLOUD_REST  # pylint:disable=no-name-in-module
 from gcloud.aio.datastore import Datastore
 from gcloud.aio.datastore import Key
 from gcloud.aio.datastore import LatLng
 from gcloud.aio.datastore import PathElement
-
-# Selectively load libraries based on the package
-if BUILD_GCLOUD_REST:
-    from requests import Session
-else:
-    from aiohttp import ClientSession as Session
 
 
 @pytest.mark.asyncio
 async def test_geo_point_value(creds: str, kind: str, project: str) -> None:
     key = Key(project, [PathElement(kind)])
 
-    async with Session(timeout=10) as s:
-        ds = Datastore(project=project, service_file=creds, session=s)
+    async with Datastore(project=project, service_file=creds) as ds:
 
-        allocatedKeys = await ds.allocateIds([key], session=s)
-        await ds.reserveIds(allocatedKeys, session=s)
+        allocatedKeys = await ds.allocateIds([key])
+        await ds.reserveIds(allocatedKeys)
 
         props_insert = {'location': LatLng(49.2827, 123.1207)}
-        await ds.insert(allocatedKeys[0], props_insert, session=s)
-        actual = await ds.lookup([allocatedKeys[0]], session=s)
+        await ds.insert(allocatedKeys[0], props_insert)
+        actual = await ds.lookup([allocatedKeys[0]])
         assert actual['found'][0].entity.properties == props_insert
