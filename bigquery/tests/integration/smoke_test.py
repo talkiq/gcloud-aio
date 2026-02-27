51aca0858ef877af6e7366f9e2a28388f6ed56a8
Kevin James <kevin@talkiq.com>
2026-02-27 18:23:37 +0000

    refactor(retry): migrate from backoff to tenacity

--- bigquery/tests/integration/smoke_test.py
+++ bigquery/tests/integration/smoke_test.py
@@ -1,6 +1,5 @@
 import uuid
 
-import backoff
 import pytest
 from gcloud.aio.auth import BUILD_GCLOUD_REST  # pylint: disable=no-name-in-module
 from gcloud.aio.bigquery import SourceFormat
@@ -9,6 +8,12 @@ from gcloud.aio.datastore import Datastore  # pylint: disable=no-name-in-module
 from gcloud.aio.datastore import Key  # pylint: disable=no-name-in-module
 from gcloud.aio.datastore import PathElement  # pylint: disable=no-name-in-module
 from gcloud.aio.storage import Storage  # pylint: disable=no-name-in-module
+from tenacity import retry
+from tenacity import retry_if_exception_type
+from tenacity import retry_if_result
+from tenacity import stop_after_attempt
+from tenacity import stop_after_delay
+from tenacity import wait_fixed
 
 # Selectively load libraries based on the package
 if BUILD_GCLOUD_REST:
@@ -56,15 +61,18 @@ async def test_table_load_copy(
 
         operation = await ds.export(export_bucket_name, kinds=[kind])
 
-        def is_processing(operation):
+        def is_processing(operation) -> bool:
             return (
                 operation
                 and operation.metadata['common']['state'] == 'PROCESSING'
             )
 
-        operation = await backoff.on_predicate(
-            backoff.constant, is_processing, interval=10, max_tries=10)(
-                ds.get_datastore_operation)(operation.name)
+        operation = await retry(
+            retry=retry_if_result(is_processing),
+            wait=wait_fixed(10),
+            stop=stop_after_attempt(10),
+            reraise=True,
+        )(ds.get_datastore_operation)(operation.name)
 
         assert operation.metadata['common']['state'] == 'SUCCESSFUL'
         # END: copy from `test_datastore_export`
@@ -87,9 +95,12 @@ async def test_table_load_copy(
             source_format=SourceFormat.DATASTORE_BACKUP,
         )
 
-        source_table = await backoff.on_exception(
-            backoff.constant, Exception, max_time=60, jitter=None,
-            interval=10)(t.get)()
+        source_table = await retry(
+            retry=retry_if_exception_type(Exception),
+            wait=wait_fixed(10),
+            stop=stop_after_delay(60),
+            reraise=True,
+        )(t.get)()
         assert int(source_table['numRows']) > 0
 
         await t.insert_via_copy(project, dataset, copy_entity_table)
@@ -97,9 +108,13 @@ async def test_table_load_copy(
             dataset, copy_entity_table, project=project,
             service_file=creds, session=s,
         )
-        copy_table = await backoff.on_exception(
-            backoff.constant, Exception, max_time=60, jitter=None,
-            interval=10)(t1.get)()
+
+        copy_table = await retry(
+            retry=retry_if_exception_type(Exception),
+            wait=wait_fixed(10),
+            stop=stop_after_delay(60),
+            reraise=True,
+        )(t1.get)()
         assert copy_table['numRows'] == source_table['numRows']
 
         # delete the backup and copy table
